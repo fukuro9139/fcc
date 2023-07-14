aab7342993952dcdffc5b71be25d79e9f8599e1d
fukuro9139 <peridot1451@gmail.com>
2023-07-15 06:13:43 +0900

    トークンを追加

--- fcc.cpp
+++ fcc.cpp
@@ -2,12 +2,64 @@
 #include<vector>
 #include<string>
 
-static auto argv_to_vector(int argc, char **argv){
+using std::cout, std::vector, std::string;
 
+/* トークンの種類 */
+enum class TokenKind {
+	TK_RESERVED,	/* 記号 */
+	TK_NUM,			/* 整数トークン */
+	TK_EOF,			/* 入力の終わりを表すトークン */
+};
+
+/* トークン型 */
+struct Token {
+	TokenKind kind;	/* トークンの型 */
+	Token *next;	/* 次の入力トークン */
+	int val;		/* kindがTK_NUMの場合、その数値 */
+	string str;		/* トークン文字列 */
+};
+
+/* 現在着目しているトークン */
+Token *token;
+
+/* エラーを報告するための関数 */
+void error(const string &err) {
+	std::cerr << err << "\n";
+	exit(1);
 }
 
+/**
+ *  次のトークンが期待している記号の時には、トークンを1つ進めてtrueを返す。
+ * それ以外の場合には偽を返す
+ */
+bool consume(const char &op) {
+	if(token->kind != TokenKind::TK_RESERVED || token->str[0] != op) {return false;}
+	token = token->next;
+	return true;
+}
+
+/**
+ * 次のトークンが期待している記号の時はトークンを１つ進める
+ * それ以外の場合にはエラーを報告する
+*/
+void expect(const char &op){
+	if (token->kind != TokenKind::TK_RESERVED || token->str[0] != op) {error(string{op}+ "ではありません");}
+	token = token->next;
+}
+
+/**
+ * 次のトークンが数値の場合、トークンを１つ読み進めてその数値を返す。
+ * それ以外の場合はエラーを返す
+*/
+int expect_number(){
+	if(token->kind != TokenKind::TK_NUM){error("数ではありません");}
+	int val = token->val;
+	token = token->next;
+}
+
+
+
 int main(int argc, char **argv){
-	using std::cout, std::vector, std::string;
 
 	if(argc != 2){
 		std::cerr << "引数の個数が正しくありません\n";
@@ -34,7 +86,7 @@ int main(int argc, char **argv){
 			cout << " sub rax, " << std::stol(args[1], &idx, 10) << "\n";
 			continue;
 		}
-		std::cerr << "予期しない文字です: " << args[1][idx];
+		std::cerr << "予期しない文字です: " << args[1][idx] << "\n";
 		return 1;
 	}
 
