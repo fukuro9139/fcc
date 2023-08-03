7fd244ffbb963e1dbc3a50dafa17ff80668a2d65
fukuro9139 <peridot1451@gmail.com>
2023-08-03 12:02:32 +0900

    テストケースを追加

--- test/arith.c
+++ test/arith.c
@@ -72,6 +72,10 @@ int main()
 	ASSERT(4, sizeof(!(char)0));
 	ASSERT(4, sizeof(!(long)0));
 
+	ASSERT(1, sizeof(~(char)0));
+	ASSERT(4, sizeof(~(int)0));
+	ASSERT(8, sizeof(~(long)0));
+
 	ASSERT(-1, ~0);
 	ASSERT(0, ~-1);
 
