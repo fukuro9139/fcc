a4ee0a4aff2cffb6132e56481fec12f19de9e320
fukuro9139 <peridot1451@gmail.com>
2023-08-13 09:39:34 +0900

    xmm0に対してmovqを使用

--- codegen.cpp
+++ codegen.cpp
@@ -343,12 +343,12 @@ void CodeGen::generate_expression(Node *node)
 		if(TypeKind::TY_FLOAT == node->_ty->_kind){
 			u.f32 = node->_fval;
 			*os << "  mov eax, " << u.u32 << " #float " << node->_fval << "\n";
-			*os << "  movss xmm0, eax\n";
+			*os << "  movq xmm0, rax\n";
 			return;
 		}else if(TypeKind::TY_DOUBLE == node->_ty->_kind){
 			u.f64 = node->_fval;
 			*os << "  mov rax, " << u.u64 << " #double " << node->_fval << "\n";
-			*os << "  movsd xmm0, rax\n";
+			*os << "  movq xmm0, rax\n";
 			return;
 		}
 
