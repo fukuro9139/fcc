b859137a64f3d650d4215c2b145ebd5e5ba2d36a
fukuro9139 <peridot1451@gmail.com>
2023-08-20 16:20:30 +0900

    asset関数をtest.hに移動

--- test/test.h
+++ test/test.h
@@ -1,8 +1,20 @@
 #define ASSERT(x, y) assert(x, y, #y)
 
-void assert(int expected, int actual, char *code);
 int printf(char *fmt, ...);
 int sprintf(char *buf, char *fmt, ...);
 int strcmp(char *p, char *q);
 int memcmp(char *p, char *q, long n);
 void exit(int n);
+
+void assert(int expected, int actual, char *code)
+{
+	if (expected == actual)
+	{
+		printf("%s => %d\n", code, actual);
+	}
+	else
+	{
+		printf("%s => %d expected but got %d\n", code, expected, actual);
+		exit(1);
+	}
+}
