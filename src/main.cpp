1a03f12d49f4f1b146a377218ffcc817bdba7ee9
fukuro9139 <peridot1451@gmail.com>
2023-08-22 01:14:01 +0900

    docgen追加

--- src/main.cpp
+++ src/main.cpp
@@ -86,6 +86,13 @@ void fcc(const unique_ptr<Input> &in, const string &input_path, const string &ou
 	CodeGen::generate_code(program, input_path, output_path, in->_opt_g);
 }
 
+/**
+ * @brief メイン処理
+ * 
+ * @param argc 
+ * @param argv 
+ * @return int 
+ */
 int main(int argc, char **argv)
 {
 	/* 入力をvectorに変換 */
